16f2f77d32ce6782b1700634cdc1292bfba30564
Luis Capuyon <33508169+frostehhh@users.noreply.github.com>
2019-04-28 14:58:22 +0800

    added placeholder and code to get uspmaintenancetables

--- MaintenanceWebUtilityWebForm2/Dynamic.aspx
+++ MaintenanceWebUtilityWebForm2/Dynamic.aspx
@@ -1,3 +1,6 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Dynamic.aspx.cs" Inherits="MaintenanceWebUtilityWebForm2.Dynamic" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <asp:PlaceHolder runat="server" id="MaintenanceNavPlaceHolder" />
+    </div>
 </asp:Content>
--- MaintenanceWebUtilityWebForm2/Dynamic.aspx.cs
+++ MaintenanceWebUtilityWebForm2/Dynamic.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,114 @@ namespace MaintenanceWebUtilityWebForm2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string constr = ConfigurationManager.ConnectionStrings["MaintenanceWebUtilityDbEntitiesDataSource"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
 
+                using (SqlCommand cmd = new SqlCommand("uspGetMaintenanceTables"))
+                {
+                    List<string> maintenanceTables = new List<string>();
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", Session[SessionKey.UserId]);
+                    cmd.Connection = con;
+                    con.Open();
+                    var maintenance = cmd.ExecuteReader();
+                    con.Close();
+                    if (maintenance.HasRows)
+                    {
+                        while (maintenance.Read())
+                        {
+                            maintenanceTables.Add(maintenance.GetString(0));
+                        }
+                    }
+
+
+
+                    //if (empAccessiblesDict[1] == true)
+                    //{
+                    //    facilityLiteral = @"<div class=""col-xl-3 col-sm-6 mb-3"">
+                    //                                    <div class=""card text-white bg-primary o-hidden h-100"">
+                    //                                        <div class=""card-body"">
+                    //                                            <div class=""card-body-icon"">
+                    //                                                <i class=""fas fa-fw fa-comments""></i>
+                    //                                            </div>
+                    //                                            <div class=""mr -5"">IT</div>
+                    //                                        </div>
+                    //                                        <a class=""card-footer text-white clearfix small z-1"" href=""#"">
+                    //                                            <span class=""float-left"">View Details</span>
+                    //                                            <span class=""float-right"">
+                    //                                                <i class=""fas fa-angle-right""></i>
+                    //                                            </span>
+                    //                                        </a>
+                    //                                    </div>
+                    //                                </div>";
+                    //    empAccessibleLiteral.Text = String.Concat(empAccessibleLiteral.Text, facilityLiteral);
+                    //}
+                    //if (empAccessiblesDict[2] == true)
+                    //{
+                    //    facilityLiteral = @"<div class=""col-xl-3 col-sm-6 mb-3"">
+                    //                                    <div class=""card text-white bg-warning o-hidden h-100"">
+                    //                                        <div class=""card-body"">
+                    //                                            <div class=""card-body-icon"">
+                    //                                                <i class=""fas fa-fw fa-comments""></i>
+                    //                                            </div>
+                    //                                            <div class=""mr -5"">SHS</div>
+                    //                                        </div>
+                    //                                        <a class=""card-footer text-white clearfix small z-1"" runat=""server"" href=""./Facilities/Index.aspx?facilityId=2"">
+                    //                                            <span class=""float-left"">View Details</span>
+                    //                                            <span class=""float-right"">
+                    //                                                <i class=""fas fa-angle-right""></i>
+                    //                                            </span>
+                    //                                        </a>
+                    //                                    </div>
+                    //                                </div>";
+                    //    empAccessibleLiteral.Text = String.Concat(empAccessibleLiteral.Text, facilityLiteral);
+                    //}
+                    //if (empAccessiblesDict[3] == true)
+                    //{
+                    //    facilityLiteral = @"<div class=""col-xl-3 col-sm-6 mb-3"">
+                    //                                    <div class=""card text-white bg-success o-hidden h-100"">
+                    //                                        <div class=""card-body"">
+                    //                                            <div class=""card-body-icon"">
+                    //                                                <i class=""fas fa-fw fa-comments""></i>
+                    //                                            </div>
+                    //                                            <div class=""mr -5"">CS</div>
+                    //                                        </div>
+                    //                                        <a class=""card-footer text-white clearfix small z-1"" href =""#"">
+                    //                                            <span class=""float-left"">View Details</span>
+                    //                                            <span class=""float-right"">
+                    //                                                <i class=""fas fa-angle-right""></i>
+                    //                                            </span>
+                    //                                        </a>
+                    //                                    </div>
+                    //                                </div>";
+                    //    empAccessibleLiteral.Text = String.Concat(empAccessibleLiteral.Text, facilityLiteral);
+                    //}
+                    //if (empAccessiblesDict[4] == true)
+                    //{
+                    //    facilityLiteral = @"<div class=""col-xl-3 col-sm-6 mb-3"">
+                    //                                    <div class=""card text-white bg-danger o-hidden h-100"">
+                    //                                        <div class=""card-body"">
+                    //                                            <div class=""card-body-icon"">
+                    //                                                <i class=""fas fa-fw fa-comments""></i>
+                    //                                            </div>
+                    //                                            <div class=""mr -5"">PSY</div>
+                    //                                        </div>
+                    //                                        <a class=""card-footer text-white clearfix small z-1"" href =""#"">
+                    //                                            <span class=""float-left"">View Details</span>
+                    //                                            <span class=""float-right"">
+                    //                                                <i class=""fas fa-angle-right""></i>
+                    //                                            </span>
+                    //                                        </a>
+                    //                                    </div>
+                    //                                </div>";
+                    //    empAccessibleLiteral.Text = String.Concat(empAccessibleLiteral.Text, facilityLiteral);
+                }
+                }
+
+
+            }
         }
     }
 }
